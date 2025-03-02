6f1fa76820c359709b60438f522a167cd93a2073
skader <sekouskaderkamara@gmail.com>
2025-03-02 11:06:54 +0000

    forms bugs fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import list_books, LibraryDetailView, CustomLogoutView, CustomLoginV
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-Based View
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),  # Class-Based View
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
