b3f50ea548936a25afca746039cafa58b809a025
skader <sekouskaderkamara@gmail.com>
2025-03-02 11:36:27 +0000

    forms auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from . import views
 from .views import list_books, LibraryDetailView, CustomLogoutView, CustomLoginView, register
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-Based View
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
