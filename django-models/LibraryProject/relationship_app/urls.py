7552e8b118181283550eb8a1663a2027ad298385
skader <sekouskaderkamara@gmail.com>
2025-03-02 11:02:12 +0000

    forms auth commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, CustomLogoutView, CustomLoginView, register
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-Based View
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-Based View
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),  # Class-Based View
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -14,3 +16,21 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  
     context_object_name = "library"
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new user to the database
+            return redirect('login')  # Redirect to the login page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+# Custom LoginView to use our login.html template
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+# Custom LogoutView to use our logout.html template
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
