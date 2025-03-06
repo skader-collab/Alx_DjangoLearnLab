14f89d03ed105b407281faa1e0829802ed5580fb
skader <sekouskaderkamara@gmail.com>
2025-03-06 22:45:03 +0000

    admin panel fix

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 # Register your models here.
 
@@ -11,3 +14,17 @@ class BookAdmin(admin.ModelAdmin):
 
     def __str__(self):
         return self.title
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+
+admin.site.register(CustomUser, CustomUserAdmin)
