b55387e91b1d5abb08ed70fbd58bedc095c3272a
skader <sekouskaderkamara@gmail.com>
2025-03-06 23:22:14 +0000

    example form commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from . models import Book
-from .forms import BookForm, ExampleForm 
+from .forms import ExampleForm 
 
 # Create your views here.
 @permission_required('myapp.can_view', raise_exception=True)
