f0502b8dce5654f615dee0655a1883f097bd0be0
Sekou <sekouskaderkamara@gmail.com>
2025-03-22 11:17:42 +0000

    django filters fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import PermissionDenied
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 
 class BookListView(generics.ListAPIView):
