c2b97302ff36203e0e3096cc6cd2d2ff13a5831b
Sekou <sekouskaderkamara@gmail.com>
2025-03-22 11:08:01 +0000

    rest permission add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.exceptions import PermissionDenied
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
