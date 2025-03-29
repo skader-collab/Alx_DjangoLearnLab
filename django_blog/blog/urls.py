a107684ed32d515a99a3f1135a4bd98c822d994e
Sekou <sekouskaderkamara@gmail.com>
2025-03-29 11:30:07 +0000

    urls for blog/urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,8 +18,10 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
     path('',PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/delete', PostDetailView.as_view(), name='post-detail'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
 ]
